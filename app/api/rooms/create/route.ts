55c9e86c93bb30e9e570441725866a6e61f6fa40
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-10-31 21:59:11 -0300

    changed the name of the application

--- app/api/rooms/create/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/lib/prismadb";
-
-export async function GET() {
-  const createdRoom = await db.chatRoom.create({
-    data: {},
-  });
-
-  return new Response(createdRoom.id);
-}
