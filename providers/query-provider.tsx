1e1c8d0bbf342caa0c5b38d69eb00d91eb67557d
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-10-31 22:21:10 -0300

    deleted nextUi imports

--- providers/query-provider.tsx
+++ providers/query-provider.tsx
@@ -1,11 +1,9 @@
 "use client";
-import { ThemeProviderProps } from "next-themes/dist/types";
 import React, { FC, ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export interface ProvidersProps {
   children: ReactNode;
-  themeProps?: ThemeProviderProps;
 }
 export const QueryProvider: FC<ProvidersProps> = ({ children }) => {
   const queryClient = new QueryClient();
