543957ccdb3ce71618e0a5b0e52ab8e3bc400aad
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-10-31 22:05:13 -0300

    in the previous commit I also deleted the older version of the app, in this commit I am changing the forms excluding the nextUi components

--- components/forms/createUser.tsx
+++ components/forms/createUser.tsx
@@ -17,7 +17,6 @@ import { signIn, useSession } from "next-auth/react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { RegisterSchema } from "@/lib/validation/auth";
-import { Card, CardBody, CardFooter } from "@nextui-org/react";
 import { useEffect } from "react";
 const RegisterForm = () => {
   const session = useSession();
@@ -67,98 +66,94 @@ const RegisterForm = () => {
   return (
     <>
       <h1 className={`text-2xl`}>Criar conta</h1>
-      <Card className="min-w-[400px] max-h-[800px] border rounded-lg p-10 pb-2 mt-6">
-        <Form {...form}>
-          <CardBody>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nome</FormLabel>
-                    <FormControl>
-                      <Input type="text" placeholder="Seu nome" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="email"
-                        placeholder="Seu melhor e-mail"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Senha</FormLabel>
-                    <FormControl>
-                      <Input type="password" placeholder="******" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div className="flex justify-center items-center">
-                <Button
-                  className="hover:text-zinc-400 w-full mt-5"
-                  variant="outline"
-                  type="submit"
-                >
-                  Registrar
-                </Button>
-              </div>
-            </form>
-          </CardBody>
-          <CardFooter className="flex justify-center items-center">
-            <div>
-              <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
-                ou
-              </div>
-              <div className="gap-3 flex flex-col justify-center items-center">
-                <Button
-                  className="hover:text-zinc-400 w-full"
-                  variant="outline"
-                  onClick={() => signIn("github")}
-                >
-                  Login com Github
-                </Button>
-                <Button
-                  className="hover:text-zinc-400 w-full"
-                  variant="outline"
-                  onClick={() => signIn("google")}
-                >
-                  Login com Google
-                </Button>
-              </div>
-              <p className="text-center text-sm text-slate-300 mt-10">
-                Você já possui uma conta?
-                <Link
-                  className="text-violet-400 hover:underline ml-2"
-                  href="/login"
-                >
-                  Login
-                </Link>
-              </p>
-            </div>
-          </CardFooter>
-        </Form>
-      </Card>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Seu nome" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Seu melhor e-mail"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="******" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-center items-center">
+            <Button
+              className="hover:text-zinc-400 w-full mt-5"
+              variant="outline"
+              type="submit"
+            >
+              Registrar
+            </Button>
+          </div>
+        </form>
+
+        <div>
+          <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+            ou
+          </div>
+          <div className="gap-3 flex flex-col justify-center items-center">
+            <Button
+              className="hover:text-zinc-400 w-full"
+              variant="outline"
+              onClick={() => signIn("github")}
+            >
+              Login com Github
+            </Button>
+            <Button
+              className="hover:text-zinc-400 w-full"
+              variant="outline"
+              onClick={() => signIn("google")}
+            >
+              Login com Google
+            </Button>
+          </div>
+          <p className="text-center text-sm text-slate-300 mt-10">
+            Você já possui uma conta?
+            <Link
+              className="text-violet-400 hover:underline ml-2"
+              href="/login"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </Form>
     </>
   );
 };
--- components/forms/loginUser.tsx
+++ components/forms/loginUser.tsx
@@ -17,7 +17,6 @@ import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { LoginSchema } from "@/lib/validation/auth";
-import { Card, CardBody, CardFooter } from "@nextui-org/react";
 
 const LoginForm = () => {
   const session = useSession();
@@ -76,85 +75,81 @@ const LoginForm = () => {
   return (
     <>
       <h1 className={`text-2xl`}>Fazer Login</h1>
-      <Card className="min-w-[400px] max-h-[800px] border rounded-lg p-2 pb-2 mt-6">
-        <Form {...form}>
-          <CardBody>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="email"
-                        placeholder="Seu melhor e-mail"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Senha</FormLabel>
-                    <FormControl>
-                      <Input type="password" placeholder="******" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div className="flex justify-center items-center">
-                <Button
-                  className="hover:text-zinc-400 w-full mt-[20px]"
-                  variant="outline"
-                  type="submit"
-                >
-                  Login
-                </Button>
-              </div>
-            </form>
-          </CardBody>
-          <CardFooter className="flex text-center items-center justify-center">
-            <div className="flex flex-col justify-center items-center">
-              <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
-                ou
-              </div>
-              <div className="gap-3 flex flex-col justify-center items-center w-full">
-                <Button
-                  className="hover:text-zinc-400 w-full"
-                  variant="outline"
-                  onClick={githubSignInHandler}
-                >
-                  Login com Github
-                </Button>
-                <Button
-                  className="hover:text-zinc-400 w-full"
-                  variant="outline"
-                  onClick={googleSignInHandler}
-                >
-                  Login com Google
-                </Button>
-              </div>
-              <p className="text-center text-sm text-slate-300 mt-20">
-                Se você não possui uma conta
-                <Link
-                  className="text-violet-400 hover:underline ml-2"
-                  href="/register"
-                >
-                  Registrar
-                </Link>
-              </p>
-            </div>
-          </CardFooter>
-        </Form>
-      </Card>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Seu melhor e-mail"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="******" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-center items-center">
+            <Button
+              className="hover:text-zinc-400 w-full mt-[20px]"
+              variant="outline"
+              type="submit"
+            >
+              Login
+            </Button>
+          </div>
+        </form>
+
+        <div className="flex flex-col justify-center items-center">
+          <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+            ou
+          </div>
+          <div className="gap-3 flex flex-col justify-center items-center w-full">
+            <Button
+              className="hover:text-zinc-400 w-full"
+              variant="outline"
+              onClick={githubSignInHandler}
+            >
+              Login com Github
+            </Button>
+            <Button
+              className="hover:text-zinc-400 w-full"
+              variant="outline"
+              onClick={googleSignInHandler}
+            >
+              Login com Google
+            </Button>
+          </div>
+          <p className="text-center text-sm text-slate-300 mt-20">
+            Se você não possui uma conta
+            <Link
+              className="text-violet-400 hover:underline ml-2"
+              href="/register"
+            >
+              Registrar
+            </Link>
+          </p>
+        </div>
+      </Form>
     </>
   );
 };
