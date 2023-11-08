685e3748860bcd869c61ebd58ef7e69d2159711f
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-11-08 20:18:12 -0300

    trying to deploy

--- app/(root)/stories/[id]/page.tsx
+++ app/(root)/stories/[id]/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
 import { db } from "@/lib/prismadb";
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import MaxWidthWrapper from "@/components/shared/MaxWidthWrapper";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 interface StoryDetailPageProps {
   params: {
@@ -7,20 +20,28 @@ interface StoryDetailPageProps {
   };
 }
 
-const StoryDetailPage: FC<StoryDetailPageProps> = async ({ params }) => {
-  async function getStory(id: string) {
-    const response = await db.story.findFirst({
-      where: {
-        id: id,
-      },
-      select: {
-        id: true,
-      },
-    });
-    return response;
-  }
-  const story = await getStory(params.id);
+async function getStory(id: string) {
+  const response = await db.story.findFirst({
+    where: {
+      id: id,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return response;
+}
 
+const StoryDetailPage: FC<StoryDetailPageProps> = async ({ params }) => {
+  const story = await getStory(params.id);
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  });
   return <section>{story?.id}</section>;
 };
 
