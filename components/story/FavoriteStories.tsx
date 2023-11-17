3e46b2ecd8ff5b33922ad0d9ca5196c6c60f35b4
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-11-16 22:05:13 -0300

    fully added the favorite funcionality

--- components/story/FavoriteStories.tsx
+++ components/story/FavoriteStories.tsx
@@ -8,18 +8,18 @@ import MaxWidthWrapper from "../shared/MaxWidthWrapper";
 import Loading from "../shared/Loading";
 
 const FavoriteStories = () => {
-  const { data: dataFavorites, isLoading: isLoadingFavoriteStories } = useQuery({
-    queryKey: ["favorites"],
-    queryFn: async () => {
-      const response = await axios.get("/api/story/favorite");
-      return response.data;
-    },
-  });
+  const { data: dataFavorites, isLoading: isLoadingFavoriteStories } = useQuery(
+    {
+      queryKey: ["favorites"],
+      queryFn: async () => {
+        const response = await axios.get("/api/story/favorite");
+        return response.data;
+      },
+    }
+  );
   console.log(dataFavorites);
 
-  const dataFavoritesTyped = dataFavorites as
-  | { favorites: Favorite[] }
-  | undefined;
+  const dataFavoritesTyped = dataFavorites as { favorites: any } | undefined;
 
   if (isLoadingFavoriteStories) {
     return (
@@ -32,19 +32,27 @@ const FavoriteStories = () => {
   return (
     <div>
       <div className="grid xl:grid-cols-3 grid-cols-1 sm:grid-cols-2 gap-4 ">
-      {dataFavoritesTyped?.favorites.map((favorite) => (
-        <StoryCard
-          key={favorite.id}
-          story={{
-            id: favorite.storyId,
-            title: favorite.storyId,
-            content: favorite.storyId,
-            coverImage: "",
-            category: favorite.storyId,
-            isCompleted: true,
-          }}
-        />
-      ))}
+        {dataFavoritesTyped?.favorites.length > 0 ? (
+          <div>
+            {dataFavoritesTyped?.favorites.map((favorite: any) => (
+              <StoryCard
+                key={favorite.id}
+                story={{
+                  id: favorite.storyId,
+                  title: favorite.story.title,
+                  content: favorite.story.content,
+                  coverImage: favorite.story.coverImage,
+                  category: favorite.story.category,
+                  isCompleted: favorite.story.isCompleted,
+                }}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-2xl font-semibold text-muted-foreground">
+            Ops, como está vazio aqui...
+          </p>
+        )}
       </div>
     </div>
   );
